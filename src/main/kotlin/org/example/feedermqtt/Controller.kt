c60df5a263e414594a60015129f4ef3a4dbfb184
KacperC948 <cichosz.kacper01@gmail.com>
2024-11-02 13:12:56 +0100

    fixes

--- src/main/kotlin/org/example/feedermqtt/Controller.kt
+++ src/main/kotlin/org/example/feedermqtt/Controller.kt
@@ -3,7 +3,7 @@ package org.example.feedermqtt
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
-@CrossOrigin(origins = ["http://localhost:3000"])
+//@CrossOrigin(origins = ["http://localhost:3000"])
 @RestController
 class Controller {
 
@@ -16,14 +16,14 @@ class Controller {
         return "blog"
     }
 
-    @CrossOrigin(origins = ["http://localhost:3000"])
+//    @CrossOrigin(origins = ["http://localhost:3000"])
     @PostMapping("/post")
     fun postMessage(@RequestBody message: String): String {
         testService.publishManually(message.toByteArray())
         return "Message published"
     }
 
-    @CrossOrigin(origins = ["http://localhost:3000"])
+//    @CrossOrigin(origins = ["http://localhost:3000"])
     @PostMapping("/times")
     fun postTimes(@RequestBody message: String): String {
         testService.publishManually(message.toByteArray())
